c2e2ea29dcc3fc5038e9ca5cdb5678e9a07681ff
TrongDang2608 <zeusgamez2608@gmail.com>
2026-02-26 19:28:46 +0700

    Tự Học: #14 Validation bằng custom annotation -- Hoàn Thành

--- src/main/java/com/devteria/identityservice/dto/request/UserUpdateRequest.java
+++ src/main/java/com/devteria/identityservice/dto/request/UserUpdateRequest.java
@@ -1,5 +1,7 @@
 package com.devteria.identityservice.dto.request;
 
+import com.devteria.identityservice.validator.DobConstraint;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -12,9 +14,14 @@ import java.util.Set;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserUpdateRequest {
+    @Size(min = 8, message = "INVALID_PASSWORD")
     String password;
+
     String firstName;
     String lastName;
+
+    @DobConstraint(min = 18, message = "INVALID_DOB")
     LocalDate dob;
+
     Set<String> roles;
 }
